9df3f7648bc41b14447714df48a98d1be009e5f6
nemhon <namhhdev024@gmail.com>
2024-11-05 17:26:37 +0700

    create Excel file

--- src/java/service/UploadExcel.java
+++ src/java/service/UploadExcel.java
@@ -60,7 +60,7 @@ public class UploadExcel extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        String d = request.getParameter("heloo");
     } 
 
     /** 
